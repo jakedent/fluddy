e28fa7b4e3a95231115a187046dba13ace3b6672
Jacob <info@jacobdent.com>
2020-02-08 21:57:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ initialize_flask_buddy()
 
 setuptools.setup(
     name='fluddy',
-    version='0.0.1',
+    version='0.0.2',
     author='Jacob Dent',
     author_email='info@jacobdent.com',
     packages=find_packages(),
@@ -47,7 +47,10 @@ setuptools.setup(
     zip_safe=True,
     long_description='''
     Flask is fast becoming the most popular web development framework for Python. Developers will typically manage multiple Flask apps individually where each app will rely on its own virtual environment - this can become tedius when launching and updating multiple Flask Apps in development. 
+    \n
     Flask Buddy (fluddy) allows developers to launch and update their individual Flask Apps through a simple Command-Line Interface. CLI functionality also allows developers to create a launchable 'Hello from Fluddy' Flask App and to add previously created Flask Apps to fluddy for painless launches and updates.
+    \n
+    Full README and usage details: https://github.com/jakedent/fluddy
     ''',
     python_requires='>=3.5',
 
